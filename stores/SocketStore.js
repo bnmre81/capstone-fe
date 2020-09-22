249be1124796d476b40e857d30c63b1f79cebc40
Hassan Alwayel <hsn.alwayel@gmail.com>
2020-09-22 16:33:00 +0300

    instant nominaaatiooooon

--- stores/SocketStore.js
+++ stores/SocketStore.js
@@ -5,19 +5,25 @@ import io from "socket.io-client";
 class SocketStore {
   socket = null;
   nominatedMovies = [];
-  users = [];
+  room = null;
 
   connect = () => {
     this.socket = io("http://10.0.2.2:8000/");
+    this.socket.on("message", ({ room, movie }) => {
+      this.nominatedMovies = [...this.nominatedMovies, movie];
+    });
   };
 
   hostRoom = ({ room, user }) => {
     this.socket.emit("join_room", { room, user });
+    this.room = room;
   };
 }
 
 decorate(SocketStore, {
   socket: observable,
+  room: observable,
+  nominatedMovies: observable,
 });
 
 const socketStore = new SocketStore();
